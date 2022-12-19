3ec6f84d95c56d7c258adf6d7a28baa2ffe86d89
guiseninha <107883094+guiseninha@users.noreply.github.com>
2022-12-19 17:21:17 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "PYTHON";
+const accessCode2 = "MINECRAFT";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("H N Y T P O", 100,50)
+    fill("lightblue")
+    text("Dica: É a maior linguagem da programação", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C I N E R A T F M", 700,150)
+    fill("lightblue")
+    text("Dica: É o maior jogo feito em java", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("B N D A O C A S D O E D", 100,250)
+    fill("lightblue")
+    text("Dica: armazena dados", 100,270)
+
+}
